77a91aab05a30da37b82050b7dc12551aff44967
Rachel Elting <rachelelting@gmail.com>
2019-10-20 22:33:11 -0500

    Updated documentation.

---
+++ Documentation/html/dir_d6e6f27c1ff69fdda3051689f4b2600a.js
@@ -0,0 +1,4 @@
+var dir_d6e6f27c1ff69fdda3051689f4b2600a =
+[
+    [ "battleship", "dir_f995fe5a8b60d51e8e0f4cfa0e46ccf3.html", "dir_f995fe5a8b60d51e8e0f4cfa0e46ccf3" ]
+];
--- Documentation/html/navtreedata.js
+++ Documentation/html/navtreedata.js
@@ -23,7 +23,7 @@ for the JavaScript code in this file
 var NAVTREE =
 [
   [ "Battleship: V2!", "index.html", [
-    [ "README", "md__c_1__users__rachel__desktop__fall_2019__e_e_c_s448__software__engineering_battleship__r_e_a_d_m_e.html", null ],
+    [ "README", "md__fall_2019__e_e_c_s448__software__engineering_battleship__r_e_a_d_m_e.html", null ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
